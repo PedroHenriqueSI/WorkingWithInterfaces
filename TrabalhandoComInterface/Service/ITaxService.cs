d8ce1366d675f8e308655cda6e2ff9544c308af5
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-09-27 08:58:17 -0300

    Initial commit

---
+++ TrabalhandoComInterface/Service/ITaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabalhandoComInterface.Service
+{
+    interface ITaxService
+    {
+        public double Tax(double amount);
+    }
+}
